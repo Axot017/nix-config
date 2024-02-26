5a67f43a350599b0113c939562fc5f43b703729f
Your Name <you@example.com>
2024-02-26 20:49:02 +0100

    21 current  2024-02-26 20:48:12  24.05.20240223.cbc4211   6.1.78                          *

--- modules/home-manager/zsh.nix
+++ modules/home-manager/zsh.nix
@@ -17,7 +17,7 @@
 	src = pkgs.fetchFromGitHub {
           owner = "zsh-users";
 	  repo = "zsh-syntax-highlighting";
-	  rev = "v0.7.0";
+	  rev = "v0.8.0";
 	  sha256 = "sha256-KLUYpUu4DHRumQZ3w59m9aTW6TBKMCXl2UcKi4uMd7w=";
 	};
       }
