2681a0bc8feb624763f4db251b3f76ba484b246e
Mateusz Ledwoń <mateuszledwon@duck.com>
2024-02-27 21:00:29 +0100

    40 current  2024-02-27 21:00:26  24.05.20240223.cbc4211   6.1.78                          *

--- modules/home-manager/alacritty.nix
+++ modules/home-manager/alacritty.nix
@@ -5,6 +5,25 @@
     alacritty = {
       enable = true;
       settings = {
+        font = {
+          size = 12.0;
+          bold = {
+            family = "JetBrainsMonoNL Nerd Font Mono";
+            style = "Bold";
+          };
+          bold_italic = {
+            family = "JetBrainsMonoNL Nerd Font Mono";
+            style = "SemiBold Italic";
+          };
+          italic = {
+            family = "JetBrainsMonoNL Nerd Font Mono";
+            style = "Italic";
+          };
+          normal = {
+            family = "JetBrainsMonoNL Nerd Font Mono";
+            style = "Regular";
+          };
+        };
         colors = {
           bright = {
             black = "#6272a4";
@@ -15,6 +34,61 @@
             red = "#ff6e6e";
             white = "#ffffff";
             yellow = "#ffffa5";
+          };
+          cursor = {
+            cursor = "CellForeground";
+            text = "CellBackground";
+          };
+          footer_bar = {
+            background = "#282a36";
+            foreground = "#f8f8f2";
+          };
+          hint = {
+            start = {
+              background = "#282a36";
+              foreground = "#f1fa8c";
+            };
+            end = {
+              background = "#f1fa8c";
+              foreground = "#282a36";
+            };
+          };
+          line_indicator = {
+            background = "None";
+            foreground = "None";
+          };
+          normal = {
+            black = "#21222c";
+            blue = "#bd93f9";
+            cyan = "#8be9fd";
+            green = "#50fa7b";
+            magenta = "#ff79c6";
+            red = "#ff5555";
+            white = "#f8f8f2";
+            yellow = "#f1fa8c";
+          };
+          primary = {
+            background = "#282a36";
+            bright_foreground = "#ffffff";
+            foreground = "#f8f8f2";
+          };
+          search = {
+            focused_match = {
+              background = "#ffb86c";
+              foreground = "#44475a";
+            };
+            matches = {
+              background = "#50fa7b";
+              foreground = "#44475a";
+            };
+          };
+          selection = {
+            background = "#44475a";
+            text = "CellForeground";
+          };
+          vi_mode_cursor = {
+            cursor = "CellForeground";
+            text = "CellBackground";
           };
         };
       };
