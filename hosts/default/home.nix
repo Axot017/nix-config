ec23f4a58c9da5e7d347820edab198bd0c65577b
Mateusz Ledwoń <mateuszledwon@duck.com>
2024-02-27 20:43:58 +0100

    38 current  2024-02-27 20:43:55  24.05.20240223.cbc4211   6.1.78                          *

--- hosts/default/home.nix
+++ hosts/default/home.nix
@@ -6,6 +6,7 @@
     ../../modules/home-manager/git.nix
     ../../modules/home-manager/zsh.nix
     ../../modules/home-manager/starship.nix
+    ../../modules/home-manager/alacritty.nix
   ];
   # Home Manager needs a bit of information about you and the paths it should
   # manage.
--- modules/home-manager/alacritty.nix
+++ modules/home-manager/alacritty.nix
@@ -2,7 +2,7 @@
 { config, pkgs, ... }:
 {
   programs = {
-    allacrity = {
+    alacritty = {
       enable = true;
       settings = {
 
