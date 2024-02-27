ae87deb3f620adcee653e0edeb5c2d10d41d4114
Mateusz Ledwoń <mateuszledwon@duck.com>
2024-02-27 20:51:40 +0100

    39 current  2024-02-27 20:51:37  24.05.20240223.cbc4211   6.1.78                          *

--- modules/home-manager/alacritty.nix
+++ modules/home-manager/alacritty.nix
@@ -5,7 +5,18 @@
     alacritty = {
       enable = true;
       settings = {
-
+        colors = {
+          bright = {
+            black = "#6272a4";
+            blue = "#d6acff";
+            cyan = "#a4ffff";
+            green = "#69ff94";
+            magenta = "#ff92df";
+            red = "#ff6e6e";
+            white = "#ffffff";
+            yellow = "#ffffa5";
+          };
+        };
       };
     };
   };
--- modules/home-manager/nix-vim/keymaps.nix
+++ modules/home-manager/nix-vim/keymaps.nix
@@ -1,15 +1,22 @@
 { pkgs, config, inputs, ... }: 
 {
   programs.nixvim.keymaps = [
+    ##########################
+    ##  Normal              ##
+    ##########################
+
+    # NvimTree
     {
+      mode = "n";
       key = "<C-n>";
       action = "<cmd>NvimTreeFindFileToggle<CR>";
       options = {
         silent = true;
       };
     }
-# Window navigation
+    # Window navigation
     {
+      mode = "n";
       key = "<C-h>";
       action = "<C-w>h";
       options = {
@@ -17,6 +24,7 @@
       };
     }
     {
+      mode = "n";
       key = "<C-j>";
       action = "<C-w>j";
       options = {
@@ -24,6 +32,7 @@
       };
     }
     {
+      mode = "n";
       key = "<C-k>";
       action = "<C-w>k";
       options = {
@@ -31,8 +40,30 @@
       };
     }
     {
+      mode = "n";
       key = "<C-l>";
       action = "<C-w>l";
+      options = {
+        silent = true;
+      };
+    }
+    ##########################
+    ##  Visual              ##
+    ##########################
+
+    # Stay in indent mode
+    {
+      mode = "v";
+      key = "<";
+      action = "<gv";
+      options = {
+        silent = true;
+      };
+    }
+    {
+      mode = "v";
+      key = ">";
+      action = ">gv";
       options = {
         silent = true;
       };
