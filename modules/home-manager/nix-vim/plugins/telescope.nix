af5ffb7715634218985004d85621e947078b6df1
Mateusz Ledwoń <mateuszledwon@duck.com>
2024-02-27 21:54:42 +0100

    45 current  2024-02-27 21:54:39  24.05.20240223.cbc4211   6.1.78                          *

--- modules/home-manager/nix-vim/plugins/telescope.nix
+++ modules/home-manager/nix-vim/plugins/telescope.nix
@@ -2,5 +2,27 @@
 {
   programs.nixvim.plugins.telescope = {
     enable = true;
+    keymaps = {
+      "<leader>ff" = {
+        action = "find_files";
+        desc = "Find files";
+      };
+      "<leader>fg" = {
+        action = "live_grep";
+        desc = "Live grep";
+      };
+      "<leader>fb" = {
+        action = "buffers";
+        desc = "Find buffer";
+      };
+      "gi" = {
+        action = "lsp_implementations";
+        desc = "Go to implementation";
+      };
+      "gr" = {
+        action = "lsp_references";
+        desc = "Go to reference";
+      };
+    };
   };
 }
