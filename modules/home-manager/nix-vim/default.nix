fb8dc50b7670db779107e6a759123372600a729f
Mateusz Ledwoń <mateuszledwon@duck.com>
2024-02-27 21:36:39 +0100

    42 current  2024-02-27 21:36:35  24.05.20240223.cbc4211   6.1.78                          *

--- modules/home-manager/nix-vim/default.nix
+++ modules/home-manager/nix-vim/default.nix
@@ -4,10 +4,11 @@
     inputs.nixvim.homeManagerModules.nixvim
     ./options.nix
     ./keymaps.nix
+    ./plugins/nvim-tree.nix
+    ./plugins/treesitter.nix
   ];
   programs.nixvim = {
     enable = true;
     colorschemes.tokyonight.enable = true;
-    plugins.nvim-tree.enable = true;
   };
 }
---
+++ modules/home-manager/nix-vim/plugins/treesitter.nix
@@ -0,0 +1,57 @@
+{ pkgs, config, inputs, ... }: 
+{
+  programs.nixvim.plugins = {
+    treesitter = {
+      enable = true;
+    };
+    treesitter-textobjects = {
+      enable = true;
+      move = {
+        gotoNextStart = {
+          "]f" = {
+            desc = "Go to next function";
+            query = "@function.outer";
+          };
+        };
+        gotoNextEnd = {
+          "]F" = {
+            desc = "Go to next function end";
+            query = "@function.outer";
+          };
+        };
+        gotoPreviousStart = {
+          "]f" = {
+            desc = "Go to previous function";
+            query = "@function.outer";
+          };
+        };
+        gotoPreviousEnd = {
+          "]F" = {
+            desc = "Go to previous function end";
+            query = "@function.outer";
+          };
+        };
+      };
+      select = {
+        keymaps = {
+          af = {
+            desc = "Select a function";
+            query = "@function.outer";
+          };
+          "if" = {
+            desc = "Select a function body";
+            query = "@function.inner";
+          };
+          ac = {
+            desc = "Select a class";
+            query = "@class.outer";
+          };
+          ic = {
+            desc = "Select a class body";
+            query = "@class.inner";
+          };
+        };
+      };
+    };
+  };
+}
