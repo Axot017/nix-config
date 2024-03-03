62ca67780a11f9491ae6ac47e6146d5ac475a2ec
Mateusz Ledwoń <mateuszledwon@duck.com>
2024-03-03 21:57:21 +0100

    92 current  2024-03-03 21:57:17  24.05.20240223.cbc4211   6.1.78                          *

--- modules/home-manager/hyprland/theme.nix
+++ modules/home-manager/hyprland/theme.nix
@@ -2,6 +2,20 @@
 {
   gtk = {
     enable = true;
+    theme = {
+      name = "Catppuccin-Macchiato-Compact-Pink-Dark";
+      package = pkgs.catppuccin-gtk.override {
+        accents = [ "pink" ];
+        size = "compact";
+        tweaks = [ "black" ];
+        variant = "macchiato";
+      };
+    };
+  };
+  xdg.configFile = {
+    "gtk-4.0/assets".source = "${config.gtk.theme.package}/share/themes/${config.gtk.theme.name}/gtk-4.0/assets";
+    "gtk-4.0/gtk.css".source = "${config.gtk.theme.package}/share/themes/${config.gtk.theme.name}/gtk-4.0/gtk.css";
+    "gtk-4.0/gtk-dark.css".source = "${config.gtk.theme.package}/share/themes/${config.gtk.theme.name}/gtk-4.0/gtk-dark.css";
   };
   qt = {
     enable = true;
