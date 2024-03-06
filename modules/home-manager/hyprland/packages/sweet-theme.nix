a195819a5398a5314454ccc32221135c26308e90
Mateusz Ledwoń <mateuszledwon@duck.com>
2024-03-06 20:50:55 +0100

    97 current  2024-03-06 20:50:51  24.05.20240223.cbc4211   6.1.78                          *

--- modules/home-manager/hyprland/packages/sweet-theme.nix
+++ modules/home-manager/hyprland/packages/sweet-theme.nix
@@ -4,7 +4,7 @@ pkgs.stdenv.mkDerivation {
   name = "sweet-theme";
   src = pkgs.fetchurl {
     url = "https://github.com/EliverLara/Sweet/releases/download/v4.0/Sweet-Ambar-Blue-Dark.zip";
-    sha256 = "";
+    sha256 = "sha256-2hb2FHWyGSowRdUnrWMJENlqRtSr2CrPtDe3DSZlP8M=";
   };
   dontUnpack = true;
   installPhase = ''
