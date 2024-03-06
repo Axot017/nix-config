37a8b88e94d25bfa9a717f73ec86fd21bf3e4532
Mateusz Ledwoń <mateuszledwon@duck.com>
2024-03-06 21:26:57 +0100

    101 current  2024-03-06 21:26:53  24.05.20240223.cbc4211   6.1.78                          *

--- modules/home-manager/hyprland/theme.nix
+++ modules/home-manager/hyprland/theme.nix
@@ -6,14 +6,13 @@ in
   gtk = {
     enable = true;
     theme = {
-      name = "Sweet";
+      name = "Sweet-Ambar-Blue-Dark";
       package = sweet;
     };
   };
   xdg.configFile = {
-    "gtk-4.0/assets".source = "${config.gtk.theme.package}/share/themes/${config.gtk.theme.name}/gtk-4.0/assets";
-    "gtk-4.0/gtk.css".source = "${config.gtk.theme.package}/share/themes/${config.gtk.theme.name}/gtk-4.0/gtk.css";
-    "gtk-4.0/gtk-dark.css".source = "${config.gtk.theme.package}/share/themes/${config.gtk.theme.name}/gtk-4.0/gtk-dark.css";
+    "gtk-4.0/gtk.css".source = "${config.gtk.theme.package}/${config.gtk.theme.name}/gtk-4.0/gtk.css";
+    "gtk-4.0/gtk-dark.css".source = "${config.gtk.theme.package}/${config.gtk.theme.name}/gtk-4.0/gtk-dark.css";
   };
   qt = {
     enable = true;
