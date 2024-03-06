56b851ad3268ddf19e98f7d699b64c2b6ebc6b5d
Mateusz Ledwoń <mateuszledwon@duck.com>
2024-03-06 21:17:03 +0100

    99 current  2024-03-06 20:54:57  24.05.20240223.cbc4211   6.1.78                          *

--- hosts/default/home.nix
+++ hosts/default/home.nix
@@ -11,6 +11,7 @@
     ../../modules/home-manager/ripgrep.nix
     ../../modules/home-manager/zoxide.nix
     ../../modules/home-manager/htop.nix
+    ../../modules/home-manager/copyq.nix
   ];
   # Home Manager needs a bit of information about you and the paths it should
   # manage.
