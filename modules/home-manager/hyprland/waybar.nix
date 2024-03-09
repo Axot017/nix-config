90b683146848bcfbaa8f37ad26ee0473f6c0a39c
Mateusz Ledwoń <mateuszledwon@duck.com>
2024-03-09 22:12:10 +0100

    111 current  2024-03-09 22:12:05  24.05.20240223.cbc4211   6.1.78                          *

--- modules/home-manager/hyprland/waybar.nix
+++ modules/home-manager/hyprland/waybar.nix
@@ -67,7 +67,6 @@
     };
     style = ''
 * {
-  border: none;
   font-family: 'JetBrainsMonoNL Nerd Font Mono', 'Symbols Nerd Font Mono';
   font-size: 16px;
   min-height: 0px;
@@ -75,6 +74,8 @@
 #clock, #pulseaudio, #network, #cpu, #memory, #disk, #temperature, #clock, #window, #workspaces {
   background-color: #11111b;
   color: #cdd6f4;
+  border-image: linear-gradient(45deg, purple, orange) 1;
+  border: 5px;
 }
 
 #clock, #pulseaudio, #network, #cpu, #memory, #disk, #temperature, #clock {
