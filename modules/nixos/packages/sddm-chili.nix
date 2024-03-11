96be9c9d8fab408aa7013bfec2bcc7837164b69b
Mateusz Ledwoń <mateuszledwon@duck.com>
2024-03-11 21:35:32 +0100

    114 current  2024-03-11 21:35:28  24.05.20240223.cbc4211   6.1.78                          *

--- modules/nixos/packages/sddm-chili.nix
+++ modules/nixos/packages/sddm-chili.nix
@@ -4,7 +4,7 @@ pkgs.stdenv.mkDerivation {
   name = "chili";
   src = pkgs.fetchurl {
     url = "https://github.com/MarianArlt/sddm-chili/archive/refs/tags/0.1.5.zip";
-    sha256 = "";
+    sha256 = "sha256-Ltzn6BrM6iDvsdqc4rVeu0oQ4hfGZ2NiYQE3lLVbgLE=";
   };
   dontUnpack = true;
   installPhase = ''
