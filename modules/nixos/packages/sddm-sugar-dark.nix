24d94bfa5e69a98142576d0879cbf58f2a3e50d3
Mateusz Ledwoń <mateuszledwon@duck.com>
2024-03-11 21:50:54 +0100

    116 current  2024-03-11 21:50:50  24.05.20240223.cbc4211   6.1.78                          *

--- modules/nixos/packages/sddm-sugar-dark.nix
+++ modules/nixos/packages/sddm-sugar-dark.nix
@@ -4,7 +4,7 @@ pkgs.stdenv.mkDerivation {
   name = "sugar-dark";
   src = pkgs.fetchurl {
     url = "https://github.com/MarianArlt/sddm-sugar-dark/archive/refs/tags/v1.2.zip";
-    sha256 = "";
+    sha256 = "sha256-ICT8EmjxgIMhjergi0/VdunE5Rw4kPzLPVeFfao0xY4=";
   };
   dontUnpack = true;
   installPhase = ''
