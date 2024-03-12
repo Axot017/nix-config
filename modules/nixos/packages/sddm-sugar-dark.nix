2fbad617aecf2b53814fa5b4f611022a2c98a9be
Mateusz Ledwoń <mateuszledwon@duck.com>
2024-03-12 07:56:23 +0100

    116 current  2024-03-11 21:50:50  24.05.20240223.cbc4211   6.1.78                          *

--- modules/nixos/packages/sddm-sugar-dark.nix
+++ modules/nixos/packages/sddm-sugar-dark.nix
@@ -9,8 +9,8 @@ pkgs.stdenv.mkDerivation {
   dontUnpack = true;
   installPhase = ''
     mkdir -p $out
-    ${pkgs.unzip}/bin/unzip $src -d $out/tmp
-    cp "$out/tmp/sddm-sugar-dark-1.2/*" $out
-    rm -r "$out/tmp"
+    ${pkgs.unzip}/bin/unzip $src -d $out
+    cp "$out/sddm-sugar-dark-1.2/*" $out
+    rm -r "$out/sddm-sugar-dark-1.2"
   '';
 }
