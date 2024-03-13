a32d57b5edafc1b09de190b55c19ff4fe3196879
Mateusz Ledwoń <mateuszledwon@duck.com>
2024-03-13 21:53:26 +0100

    123 current  2024-03-13 21:53:22  24.05.20240223.cbc4211   6.1.78                          *

--- modules/home-manager/hyprland/packages/sweet-cursors.nix
+++ modules/home-manager/hyprland/packages/sweet-cursors.nix
@@ -4,7 +4,7 @@ pkgs.stdenv.mkDerivation {
   name = "sweet-theme";
   src = pkgs.fetchurl {
     url = "https://files04.pling.com/api/files/download/j/eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6IjE1OTE5OTM4NTUiLCJ1IjpudWxsLCJsdCI6ImRvd25sb2FkIiwicyI6IjVkNGFiZTUyMGFmMzllYTQ5NTNjMGRiOWViZTY1ZWY2YmMxOWYzZTllYWUwNzg0MTAzNmYxMzE0MThiYzNlZDc4Mjc5OTIxNDA1YjJkZmQxMzBlZGViMTU1YWIxYzVhNDhiNmEzYWIwODRmNjUzNWI0M2FjNmI1MDMzZDQ1YmJkIiwidCI6MTcxMDM2MzIwMywic3RmcCI6bnVsbCwic3RpcCI6bnVsbH0.-XIVv_1rJ06CvBb6mreyFnc_EKZWuhDAD6K1RHbNCdQ/Sweet-cursors.tar.xz";
-    sha256 = "";
+    sha256 = "sha256-k6gTzzIMUFe9JZu0PgM93YMpqzGhzFCTwy2GY3KYPIw=";
   };
   dontUnpack = true;
   installPhase = ''
