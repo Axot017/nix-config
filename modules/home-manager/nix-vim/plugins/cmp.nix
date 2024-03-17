ed84b3adf5ec7d5dd122fb0b1aa071b0593088f4
Mateusz Ledwoń <mateuszledwon@duck.com>
2024-03-17 12:33:09 +0100

    139 current  2024-03-17 12:33:04  24.05.20240314.d691274   6.6.21                          *

--- modules/home-manager/nix-vim/plugins/cmp.nix
+++ modules/home-manager/nix-vim/plugins/cmp.nix
@@ -3,69 +3,71 @@
   programs.nixvim.plugins = {
     cmp = {
       enable = true;
-      settings.sources = [
-          { name = "nvim_lsp"; }
-          { name = "path"; }
-          { name = "luasnip"; }
-      ];
-      # snippet.expand = ''
-      #   function(args)
-      #     require('luasnip').lsp_expand(args.body)
-      #   end
-      # '';
-      # mapping = {
-      #   "<CR>" = "cmp.mapping.confirm({ select = true })";
-      #   "<Tab>" = ''cmp.mapping(function(fallback)
-      #     if cmp.visible() then
-      #       cmp.select_next_item()
-      #     elseif luasnip.expandable() then
-      #       luasnip.expand()
-      #     elseif luasnip.expand_or_jumpable() then
-      #       luasnip.expand_or_jump()
-      #     elseif check_backspace() then
-      #       fallback()
-      #     else
-      #       fallback()
-      #     end
-      #   end, {
-      #     "i",
-      #     "s",
-      #     "c",
-      #   })'';
-      #   "<S-Tab>" = ''cmp.mapping(function(fallback)
-      #     if cmp.visible() then
-      #       cmp.select_prev_item()
-      #     elseif luasnip.jumpable(-1) then
-      #       luasnip.jump(-1)
-      #     else
-      #       fallback()
-      #     end
-      #   end, {
-      #     "i",
-      #     "s",
-      #     "c",
-      #   })'';
-      # };
-      # formatting = {
-      #   fields = [
-      #     "kind"
-      #     "abbr"
-      #     "menu"
-      #   ];
-      #   format = ''
-      #     function(entry, vim_item)
-      #       vim_item.kind = string.format("%s", kind_icons[vim_item.kind])
-      #       vim_item.menu = ({
-      #         nvim_lsp = "[LSP]",
-      #         nvim_lua = "[NVIM_LUA]",
-      #         luasnip = "[Snippet]",
-      #         buffer = "[File]",
-      #         path = "[Path]",
-      #       })[entry.source.name]
-      #       return vim_item
-      #     end
-      #   '';
-      # };
+      settings = {
+        sources = [
+            { name = "nvim_lsp"; }
+            { name = "path"; }
+            { name = "luasnip"; }
+        ];
+        snippet.expand = ''
+          function(args)
+            require('luasnip').lsp_expand(args.body)
+          end
+        '';
+        mapping = {
+          "<CR>" = "cmp.mapping.confirm({ select = true })";
+          "<Tab>" = ''cmp.mapping(function(fallback)
+            if cmp.visible() then
+              cmp.select_next_item()
+            elseif luasnip.expandable() then
+              luasnip.expand()
+            elseif luasnip.expand_or_jumpable() then
+              luasnip.expand_or_jump()
+            elseif check_backspace() then
+              fallback()
+            else
+              fallback()
+            end
+          end, {
+            "i",
+            "s",
+            "c",
+          })'';
+          "<S-Tab>" = ''cmp.mapping(function(fallback)
+            if cmp.visible() then
+              cmp.select_prev_item()
+            elseif luasnip.jumpable(-1) then
+              luasnip.jump(-1)
+            else
+              fallback()
+            end
+          end, {
+            "i",
+            "s",
+            "c",
+          })'';
+        };
+        formatting = {
+          fields = [
+            "kind"
+            "abbr"
+            "menu"
+          ];
+          format = ''
+            function(entry, vim_item)
+              vim_item.kind = string.format("%s", kind_icons[vim_item.kind])
+              vim_item.menu = ({
+                nvim_lsp = "[LSP]",
+                nvim_lua = "[NVIM_LUA]",
+                luasnip = "[Snippet]",
+                buffer = "[File]",
+                path = "[Path]",
+              })[entry.source.name]
+              return vim_item
+            end
+          '';
+        };
+      };
     };
   };
 }
