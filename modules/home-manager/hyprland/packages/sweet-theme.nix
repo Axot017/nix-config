0d305f4fecd7e580ae26493542d5a0337bcf76de
Mateusz Ledwoń <mateuszledwon@duck.com>
2024-03-17 12:03:43 +0100

    131 current  2024-03-15 22:34:56  24.05.20240223.cbc4211   6.1.78                          *

--- modules/home-manager/hyprland/packages/sweet-theme.nix
+++ modules/home-manager/hyprland/packages/sweet-theme.nix
@@ -6,7 +6,7 @@ pkgs.stdenv.mkDerivation {
     owner = "EliverLara";
     repo = "Sweet";
     rev = "0f79dedc505d946f8c1c57e0ce0c7da16a7bdee0";
-    sha256 = "";
+    sha256 = "sha256-v6aXcCDHGcoj/BOkQJ1xQdOsKvJvXkrrc7pIxgVkJX8=";
   };
   dontUnpack = true;
   installPhase = ''
