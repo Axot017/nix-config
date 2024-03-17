b3784f2be2375013751e0a6d6a7d79a2b495c584
Mateusz Ledwoń <mateuszledwon@duck.com>
2024-03-17 16:18:36 +0100

    149 current  2024-03-17 15:58:16  24.05.20240314.d691274   6.6.21                          *

--- modules/home-manager/nix-vim/plugins/lualine.nix
+++ modules/home-manager/nix-vim/plugins/lualine.nix
@@ -3,12 +3,5 @@
     enable = true;
     theme = "horizon";
     extensions = [ "nvim-tree" "nvim-dap-ui" "toggleterm" ];
-    sections = {
-      lualine_a = {
-        name = "tabs";
-        mode = 1;
-        path = 0;
-      };
-    };
   };
 }
