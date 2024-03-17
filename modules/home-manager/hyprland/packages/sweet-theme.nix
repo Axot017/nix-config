f7902ed377a27ec6e60dd8ab5127293496181ec0
Mateusz Ledwoń <mateuszledwon@duck.com>
2024-03-17 12:09:05 +0100

    132 current  2024-03-17 12:07:08  24.05.20240314.d691274   6.6.21                          *

--- modules/home-manager/hyprland/packages/sweet-theme.nix
+++ modules/home-manager/hyprland/packages/sweet-theme.nix
@@ -8,9 +8,4 @@ pkgs.stdenv.mkDerivation {
     rev = "0f79dedc505d946f8c1c57e0ce0c7da16a7bdee0";
     sha256 = "sha256-v6aXcCDHGcoj/BOkQJ1xQdOsKvJvXkrrc7pIxgVkJX8=";
   };
-  dontUnpack = true;
-  installPhase = ''
-    mkdir -p $out
-    cp -r $src $out
-  '';
 }
