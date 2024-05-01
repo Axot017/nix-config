69d761d3d1f50ec097c8ca9554f925074475d57a
Mateusz Ledwoń <mateuszledwon@duck.com>
2024-05-01 21:57:00 +0200

    196 current  2024-05-01 21:56:51  24.05.20240427.58a1abd   6.6.28                          *

--- modules/home-manager/starship.nix
+++ modules/home-manager/starship.nix
@@ -1,14 +1,18 @@
-{ config, pkgs, lib, ... }:
-{
+{ config, pkgs, lib, ... }: {
   programs.starship = {
     enable = true;
     enableZshIntegration = true;
     settings = {
       character = {
-      	success_symbol = "[λ](bold cyan)";
-      	error_symbol = "[λ](bold red)";
+        success_symbol = "[λ](bold cyan)";
+        error_symbol = "[λ](bold red)";
+        vimcmd_symbol = "[V](bold cyan)";
+        vimcmd_replace_one_symbol = "[V](bold purple)";
+        vimcmd_replace_symbol = "[V](bold purple)";
+        vimcmd_visual_symbol = "[V](bold yellow)";
       };
-      aws.format = "\\[[$symbol($profile)(\\($region\\))(\\[$duration\\])]($style)\\]";
+      aws.format =
+        "\\[[$symbol($profile)(\\($region\\))(\\[$duration\\])]($style)\\]";
       cmake.format = "\\[[$symbol($version)]($style)\\]";
       cmd_duration.format = "\\[[⏱ $duration ]($style)\\]";
       conda.format = "\\[[$symbol$environment]($style)\\]";
@@ -17,10 +21,12 @@
       deno.format = "\\[[$symbol($version)]($style)\\]";
       docker_context.format = "\\[[$symbol$context]($style)\\]";
       dotnet.format = "\\[[$symbol($version)(🎯 $tfm)]($style)\\]";
-      elixir.format = "\\[[$symbol($version \\(OTP $otp_version\\))]($style)\\]";
+      elixir.format =
+        "\\[[$symbol($version \\(OTP $otp_version\\))]($style)\\]";
       elm.format = "\\[[$symbol($version)]($style)\\]";
       erlang.format = "\\[[$symbol($version)]($style)\\]";
-      gcloud.format = "\\[[$symbol$account(@$domain)(\\($region\\))]($style)\\]";
+      gcloud.format =
+        "\\[[$symbol$account(@$domain)(\\($region\\))]($style)\\]";
       git_branch.format = "\\[[$symbol$branch]($style)\\]";
       git_status.format = "([\\[$all_status$ahead_behind\\]]($style))";
       golang.format = "\\[[$symbol($version)]($style)\\]";
@@ -35,13 +41,15 @@
       nim.format = "\\[[$symbol($version)]($style)\\]";
       nix_shell.format = "\\[[$symbol$state( \\($name\\))]($style)\\]";
       nodejs.format = "\\[[$symbol($version)]($style)\\]";
-      ocaml.format = "\\[[$symbol($version)(\\($switch_indicator$switch_name\\))]($style)\\]";
+      ocaml.format =
+        "\\[[$symbol($version)(\\($switch_indicator$switch_name\\))]($style)\\]";
       openstack.format = "\\[[$symbol$cloud(\\($project\\))]($style)\\]";
       package.format = "\\[[$symbol$version]($style)\\]";
       perl.format = "\\[[$symbol($version)]($style)\\]";
       php.format = "\\[[$symbol($version)]($style)\\]";
       purescript.format = "\\[[$symbol($version)]($style)\\]";
-      python.format = "\\[[$symbol$pyenv_prefix($version)(\\($virtualenv\\))]($style)\\]";
+      python.format =
+        "\\[[$symbol$pyenv_prefix($version)(\\($virtualenv\\))]($style)\\]";
       red.format = "\\[[$symbol($version)]($style)\\]";
       ruby.format = "\\[[$symbol($version)]($style)\\]";
       rust.format = "\\[[$symbol($version)]($style)\\]";
@@ -54,65 +62,65 @@
       vlang.format = "\\[[$symbol($version)]($style)\\]";
       zig.format = "\\[[$symbol($version)]($style)\\]";
       format = lib.concatStrings [
-      	"[┌](bold cyan) $username"
-	"$hostname"
-	"$shlvl"
-	"$kubernetes"
-	"$directory"
-	"$vcsh"
-	"$git_branch"
-	"$git_commit"
-	"$git_state"
-	"$git_metrics"
-	"$git_status"
-	"$hg_branch"
-	"$docker_context"
-	"$package"
-	"$cmake"
-	"$dart"
-	"$deno"
-	"$dotnet"
-	"$elixir"
-	"$elm"
-	"$erlang"
-	"$golang"
-	"$helm"
-	"$java"
-	"$julia"
-	"$kotlin"
-	"$nim"
-	"$nodejs"
-	"$ocaml"
-	"$perl"
-	"$php"
-	"$purescript"
-	"$python"
-	"$red"
-	"$ruby"
-	"$rust"
-	"$scala"
-	"$swift"
-	"$terraform"
-	"$vlang"
-	"$vagrant"
-	"$zig"
-	"$nix_shell"
-	"$conda"
-	"$memory_usage"
-	"$aws"
-	"$openstack"
-	"$env_var"
-	"$crystal"
-	"$custom"
-	"$cmd_duration"
-	"$line_break"
-	"$lua"
-	"$jobs"
-	"$battery"
-	"$time"
-	"$status"
-	"$shell"
-	"[└](bold cyan) $character"
+        "[┌](bold cyan) $username"
+        "$hostname"
+        "$shlvl"
+        "$kubernetes"
+        "$directory"
+        "$vcsh"
+        "$git_branch"
+        "$git_commit"
+        "$git_state"
+        "$git_metrics"
+        "$git_status"
+        "$hg_branch"
+        "$docker_context"
+        "$package"
+        "$cmake"
+        "$dart"
+        "$deno"
+        "$dotnet"
+        "$elixir"
+        "$elm"
+        "$erlang"
+        "$golang"
+        "$helm"
+        "$java"
+        "$julia"
+        "$kotlin"
+        "$nim"
+        "$nodejs"
+        "$ocaml"
+        "$perl"
+        "$php"
+        "$purescript"
+        "$python"
+        "$red"
+        "$ruby"
+        "$rust"
+        "$scala"
+        "$swift"
+        "$terraform"
+        "$vlang"
+        "$vagrant"
+        "$zig"
+        "$nix_shell"
+        "$conda"
+        "$memory_usage"
+        "$aws"
+        "$openstack"
+        "$env_var"
+        "$crystal"
+        "$custom"
+        "$cmd_duration"
+        "$line_break"
+        "$lua"
+        "$jobs"
+        "$battery"
+        "$time"
+        "$status"
+        "$shell"
+        "[└](bold cyan) $character"
       ];
     };
   };
