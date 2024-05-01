143da489e08094bfe4878341439e83f83a3d4a51
Mateusz Ledwoń <mateuszledwon@duck.com>
2024-05-01 21:35:33 +0200

    192 current  2024-05-01 21:07:28  24.05.20240427.58a1abd   6.6.28                          *

--- flake.nix
+++ flake.nix
@@ -7,7 +7,7 @@
 
     home-manager = {
       url = "github:nix-community/home-manager";
-      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.nixpkgs.follows = "nixpkgs-stable";
     };
     nixvim = {
       url = "github:nix-community/nixvim";
