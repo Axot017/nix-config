f8e979bfd38c67633f107dfc3cec00f5661fa249
Mateusz Ledwoń <mateuszledwon@duck.com>
2024-05-01 21:48:51 +0200

    194 current  2024-05-01 21:48:33  24.05.20240427.58a1abd   6.6.28                          *

--- modules/nixos/xserver.nix
+++ modules/nixos/xserver.nix
@@ -3,6 +3,7 @@
     enable = true;
     displayManager.sddm = {
       enable = true;
+      wayland.enable = true;
       # theme = "${import ./packages/sddm-chili.nix { inherit pkgs; }}";
     };
   };
