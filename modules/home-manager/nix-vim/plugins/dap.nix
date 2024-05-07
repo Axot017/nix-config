c9093068d5c29f0a2a95e030d1f498764f84d7d4
Mateusz Ledwoń <mateuszledwon@duck.com>
2024-05-07 22:03:15 +0200

    206 current  2024-05-07 22:03:05  24.05.20240427.58a1abd   6.6.28                          *

--- modules/home-manager/nix-vim/plugins/dap.nix
+++ modules/home-manager/nix-vim/plugins/dap.nix
@@ -3,17 +3,58 @@
     enable = true;
     adapters = { executables = { lldb = { command = "lldb-vscode"; }; }; };
     configurations = {
-      rust = [{
-        name = "Launch rust";
-        type = "lldb";
-        request = "launch";
-        cwd = "\${workspaceFolder}";
-        program.__raw = ''
-          function()
-            return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
-          end
-        '';
-      }];
+      rust = [
+        {
+          name = "Launch - default";
+          type = "lldb-dap";
+          request = "launch";
+          cwd = "\${workspaceFolder}";
+          initCommands = [ "cargo build" ];
+          program.__raw = ''
+            function()
+              local cwd = vim.fn.getcwd()
+              local target = vim.fn.split(cwd, '/')
+              local len = #target
+              local last = target[len]
+              return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/target/debug/' .. last, 'file')
+            end
+          '';
+        }
+        {
+          name = "Launch - select";
+          type = "lldb-dap";
+          request = "launch";
+          cwd = "\${workspaceFolder}";
+          initCommands = [ "cargo build" ];
+          program.__raw = ''
+            function()
+              local possible_files  = vim.split(vim.fn.glob(vim.fn.getcwd() .. '/target/debug/*'), '\n', {trimempty=true})
+
+              local files = {}
+              for _, file in ipairs(possible_files) do
+                if vim.fn.isdirectory(file) == 0 and vim.fn.matchstr(file, '.*%.') == "" then
+                  table.insert(files, file)
+                end
+              end
+
+              local choice = vim.fn.inputlist(files)
+              return vim.fn.getcwd() .. '/target/debug/' .. choice
+            end
+          '';
+        }
+        {
+          name = "Attach";
+          type = "lldb-dap";
+          request = "launch";
+          cwd = "\${workspaceFolder}";
+          initCommands = [ "cargo build" ];
+          program.__raw = ''
+            function()
+              return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/target/debug/', 'file')
+            end
+          '';
+        }
+      ];
     };
     extensions = {
       dap-ui = { enable = true; };
