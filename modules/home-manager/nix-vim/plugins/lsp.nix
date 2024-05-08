ce2f6a1afdf4d2310a3ee8506486b6f507472733
Mateusz Ledwoń <mateuszledwon@duck.com>
2024-05-08 21:49:54 +0200

    212 current  2024-05-08 21:49:44  24.05.20240427.58a1abd   6.6.28                          *

--- modules/home-manager/nix-vim/plugins/lsp.nix
+++ modules/home-manager/nix-vim/plugins/lsp.nix
@@ -14,7 +14,7 @@
       nil_ls = { enable = true; };
       gleam = {
         enable = true;
-        cmd = [ "~/Projects/gleam/target/debug/gleam" ];
+        cmd = [ "~/Projects/gleam/target/debug/gleam" "lsp" ];
       };
       gopls = { enable = true; };
       html = { enable = true; };
