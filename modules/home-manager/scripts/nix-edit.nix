9efa056c9d2cd12a79571c62ce4fbae44ac76945
Mateusz Ledwoń <mateuszledwon@duck.com>
2024-07-07 15:40:19 +0200

    6 current   2024-07-07 15:40:16  24.05.20240427.58a1abd   6.6.28                          *

--- modules/home-manager/scripts/nix-edit.nix
+++ modules/home-manager/scripts/nix-edit.nix
@@ -1,21 +1,22 @@
 { pkgs, ... }:
+let
+  app = pkgs.writeShellApplication {
+    name = "nix-edit-2";
+    text = ''
+      set -e
+      pushd ~/Projects/nix-config
+      nvim .
+      git diff -U0 *.nix
+      git add -A
+      echo "NixOS - rebuilding..."
+      sudo nixos-rebuild switch --flake .#default &>nixos-switch.log || cat nixos-switch.log
 
-pkgs.writeShellApplication {
-  name = "nix-edit-2";
-  text = ''
-    set -e
-    pushd ~/Projects/nix-config
-    nvim .
-    git diff -U0 *.nix
-    git add -A
-    echo "NixOS - rebuilding..."
-    sudo nixos-rebuild switch --flake .#default &>nixos-switch.log || cat nixos-switch.log
+      gen=$(nixos-rebuild list-generations --flake .#default | grep current)
+      git commit -m "$gen"
 
-    gen=$(nixos-rebuild list-generations --flake .#default | grep current)
-    git commit -m "$gen"
+      echo "NixOS - build finished"
 
-    echo "NixOS - build finished"
-
-    popd
-  '';
-}
+      popd
+    '';
+  };
+in { }
