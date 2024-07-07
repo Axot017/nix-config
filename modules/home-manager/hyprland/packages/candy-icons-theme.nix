1ab66945c33acccfd1dce42d1314cd0e7393ac0e
Mateusz Ledwoń <mateuszledwon@duck.com>
2024-07-07 13:48:39 +0200

    3 current   2024-07-06 17:39:37  24.05.20240427.58a1abd   6.6.28                          *

--- modules/home-manager/hyprland/packages/candy-icons-theme.nix
+++ modules/home-manager/hyprland/packages/candy-icons-theme.nix
@@ -2,10 +2,11 @@
 
 pkgs.stdenv.mkDerivation {
   name = "candy-icons-theme";
-  src = pkgs.fetchurl {
-    url =
-      "https://github.com/EliverLara/candy-icons/archive/refs/heads/master.zip";
-    sha256 = "sha256-wrsRoR7Ff7lWBc7ytn8omnP9uBmrXhxjlTyf3i+l2lI=";
+  src = pkgs.fetchFromGitHub {
+    owner = "EliverLara";
+    repo = "candy-icons";
+    rev = "3b36e89485daab8845f5793f9cbd2bc2ae2e303d";
+    sha256 = "sha256-v6aXcCDHGcoj/BOkQJ1xQdOsKvJvXkrrc7pIxgVkJX8=";
   };
   dontUnpack = true;
   installPhase = ''
