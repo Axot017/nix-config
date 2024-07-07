771f40a7b6ac6857feb80939431a6a72ea67c136
Mateusz Ledwoń <mateuszledwon@duck.com>
2024-07-07 13:50:41 +0200

    4 current   2024-07-07 13:50:39  24.05.20240427.58a1abd   6.6.28                          *

--- modules/home-manager/hyprland/packages/candy-icons-theme.nix
+++ modules/home-manager/hyprland/packages/candy-icons-theme.nix
@@ -8,10 +8,8 @@ pkgs.stdenv.mkDerivation {
     rev = "3b36e89485daab8845f5793f9cbd2bc2ae2e303d";
     sha256 = "sha256-v6aXcCDHGcoj/BOkQJ1xQdOsKvJvXkrrc7pIxgVkJX8=";
   };
-  dontUnpack = true;
   installPhase = ''
     mkdir -p $out
-    ${pkgs.unzip}/bin/unzip $src -d $out/
-    mv $out/candy-icons-master $out/Candy-Icons
+    cp -r $src $out/Candy-Icons
   '';
 }
