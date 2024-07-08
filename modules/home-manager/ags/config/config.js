5d1d433942d67926a5da55d81add4dace2ee7c87
Mateusz Ledwoń <mateuszledwon@duck.com>
2024-07-08 21:33:46 +0200

    37 current  2024-07-08 21:33:42  24.11.20240703.9f4128e   6.6.36                          *

--- modules/home-manager/ags/config/config.js
+++ modules/home-manager/ags/config/config.js
@@ -1,26 +1,32 @@
 const time = Variable('', {
-    poll: [1000, function() {
-        return Date().toString()
-    }],
+  poll: [1000, function() {
+    return Date().toString()
+  }],
+})
+
+const Time = () => Widget.Label({
+  hpack: 'center',
+  label: time.bind(),
 })
 
 const Bar = (/** @type {number} */ monitor) => Widget.Window({
-    monitor,
-    name: `bar${monitor}`,
-    anchor: ['top', 'left', 'right'],
-    exclusivity: 'exclusive',
-    child: Widget.CenterBox({
-        start_widget: Widget.Label({
-            hpack: 'center',
-            label: 'Welcome to AGS!',
-        }),
-        end_widget: Widget.Label({
-            hpack: 'center',
-            label: time.bind(),
-        }),
+  monitor,
+  name: `bar${monitor}`,
+  anchor: ['top', 'left', 'right'],
+  exclusivity: 'exclusive',
+  child: Widget.CenterBox({
+    start_widget: Widget.Label({
+      hpack: 'center',
+      label: 'Welcome to AGS!',
     }),
+    center_widget: Time(),
+    end_widget: Widget.Label({
+      hpack: 'center',
+      label: time.bind(),
+    }),
+  }),
 })
 
 App.config({
-    windows: [Bar(0)],
+  windows: [Bar(0)],
 })
