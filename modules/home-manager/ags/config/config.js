31ef5967a9b5fb6aeeb3604434e375007987620c
Mateusz Ledwoń <mateuszledwon@duck.com>
2024-07-09 21:50:35 +0200

    45 current  2024-07-09 21:50:31  24.11.20240703.9f4128e   6.6.36                          *

--- modules/home-manager/ags/config/config.js
+++ modules/home-manager/ags/config/config.js
@@ -12,12 +12,56 @@ const time = Variable('', {
   }],
 })
 
+const cpu = Variable('', {
+  poll: [1000, ["bash", "-c", "top -bn1 | grep Cpu | awk '{print $2}'"], out => (+out).toString()],
+})
+
 const disk = Variable('', {
-  poll: [1000, ["bash", "-c", "df / | grep / | awk '{print 0 + $5}'"], out => out.toString().trim()],
+  poll: [1000, ["bash", "-c", "df / | grep / | awk '{print 0 + $5}'"], out => (+out).toString()],
 })
 
 const mem = Variable('', {
-  poll: [1000, ["bash", "-c", "free | grep Mem | awk '{print 100 - ($7/$2 * 100)}'"], out => out.toString().trim()],
+  poll: [1000, ["bash", "-c", "free | grep Mem | awk '{print 100 - ($7/$2 * 100)}'"], out => (+out).toString()],
+})
+
+const Center = () => Widget.Box({
+  class_name: 'center',
+  children: [
+    Cpu(),
+    Mem(),
+    Disk(),
+    Time(),
+  ],
+})
+
+const Disk = () => Widget.Box({
+  class_name: 'disk',
+  children: [
+    Widget.Label({
+      hpack: 'center',
+      label: disk.bind(),
+    }),
+  ],
+})
+
+const Mem = () => Widget.Box({
+  class_name: 'mem',
+  children: [
+    Widget.Label({
+      hpack: 'center',
+      label: mem.bind(),
+    }),
+  ],
+})
+
+const Cpu = () => Widget.Box({
+  class_name: 'cpu',
+  children: [
+    Widget.Label({
+      hpack: 'center',
+      label: cpu.bind(),
+    }),
+  ],
 })
 
 const Time = () => Widget.Box({
