deb9feac1648cbb67f5e401bc653637967eb9986
Mateusz Ledwoń <mateuszledwon@duck.com>
2024-07-14 19:19:14 +0200

    116 current  2024-07-14 19:19:08  24.11.20240703.9f4128e   6.6.36                          *

--- modules/home-manager/ags/config/config.js
+++ modules/home-manager/ags/config/config.js
@@ -1,10 +1,9 @@
 import { TopBar } from "./topbar.js"
 import { NotificationPopups } from "./notifications.js"
-import { AppLauncher } from "./applauncher.js"
 
 App.config({
   style: "./style.css",
-  windows: [TopBar(0), NotificationPopups(0), AppLauncher()],
+  windows: [TopBar(0), NotificationPopups(0)],
 })
 
 export { }
