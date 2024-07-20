17fe4263ea05e3c3187b9ad483a6e001450b899c
Mateusz Ledwoń <mateuszledwon@duck.com>
2024-07-20 12:02:11 +0200

    137 current  2024-07-20 12:02:04  24.11.20240703.9f4128e   6.6.36                          *

--- modules/home-manager/hyprland/ags/config/topbar.js
+++ modules/home-manager/hyprland/ags/config/topbar.js
@@ -40,7 +40,51 @@ const FocusedTitle = () => Widget.Box({
 
 const weather = Variable({
   temp: "",
+  icon: ""
 })
+
+const mapWeatherIcon = (/** @type {string} */ icon) => {
+  switch (icon) {
+    case "01d":
+      return ""
+    case "01n":
+      return ""
+    case "02d":
+      return ""
+    case "02n":
+      return ""
+    case "03d":
+      return ""
+    case "03n":
+      return ""
+    case "04d":
+      return ""
+    case "04n":
+      return ""
+    case "09d":
+      return ""
+    case "09n":
+      return ""
+    case "10d":
+      return ""
+    case "10n":
+      return ""
+    case "11d":
+      return ""
+    case "11n":
+      return ""
+    case "13d":
+      return ""
+    case "13n":
+      return ""
+    case "50d":
+      return ""
+    case "50n":
+      return ""
+    default:
+      return ""
+  }
+};
 
 Utils.interval(900000, async () => {
   try {
@@ -56,6 +100,7 @@ Utils.interval(900000, async () => {
 
     weather.setValue({
       temp: Math.floor(body.main.temp).toString() + "°C",
+      icon: mapWeatherIcon(body.weather[0].icon)
     })
   } catch (e) {
   }
