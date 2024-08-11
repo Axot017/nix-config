14473cd8276fd60205e8d20369a717e4c70d0425
Mateusz Ledwoń <mateuszledwon@duck.com>
2024-08-11 18:36:48 +0200

    215 current  2024-08-11 18:36:40  24.11.20240703.9f4128e   6.6.36                          *

--- hosts/default/configuration.nix
+++ hosts/default/configuration.nix
@@ -180,4 +180,8 @@ in {
   # Before changing this value read the documentation for this option
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
   system.stateVersion = "23.11"; # Did you read the comment?
+
+  environment.etc."hunspell/hunspell.conf".text = ''
+    DICTIONARY pl_PL,en_US
+  '';
 }
