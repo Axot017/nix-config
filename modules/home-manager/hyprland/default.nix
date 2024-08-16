20549154ffd6778dc88d239b82c2a3ece9b5597c
Mateusz Ledwoń <mateuszledwon@duck.com>
2024-08-16 21:35:49 +0200

    235 current  2024-08-16 21:35:39  24.11.20240703.9f4128e   6.6.36                          *

--- modules/home-manager/hyprland/default.nix
+++ modules/home-manager/hyprland/default.nix
@@ -33,7 +33,7 @@ let
     kill $(cat /tmp/screenrecording.pid) && rm /tmp/screenrecording.pid
     notificationResult=$(timeout 30s notify-send -A "GOTO=Open" -t 30000 "Video saved")
     if [ "$notificationResult" = "GOTO" ]; then
-      yazi /home/axot/Videos/ScreenRecordings
+      wezterm start --new-tab yazi /home/axot/Videos/ScreenRecordings
     fi
   '';
 in {
