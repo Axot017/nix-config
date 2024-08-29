457c2b05c475bd62cab03b35c65377e60734aa2a
Mateusz Ledwoń <mateuszledwon@duck.com>
2024-08-29 23:08:02 +0200

    245 current  2024-08-29 23:07:41  24.11.20240828.71e91c4   6.6.47                          *

--- modules/home-manager/wezterm.nix
+++ modules/home-manager/wezterm.nix
@@ -8,6 +8,7 @@
         local config = wezterm.config_builder()
 
         config.enable_wayland = false
+        config.front_end = "WebGpu"
         config.font = wezterm.font("JetBrainsMonoNL Nerd Font Mono")
 
         return config
