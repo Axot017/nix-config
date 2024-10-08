6388e81ecb0024277e02744573a3763918f14a9d
Mateusz Ledwoń <mateuszledwon@duck.com>
2024-10-08 21:44:37 +0200

    274 current  2024-10-08 21:44:26  24.11.20241004.bc947f5   6.6.53                          *

--- modules/nixos/stylix.nix
+++ modules/nixos/stylix.nix
@@ -4,5 +4,11 @@
     image = ./wallpapers/witcher.jpg;
     base16Scheme = "${pkgs.base16-schemes}/share/themes/tokyo-night-storm.yaml";
     polarity = "dark";
+    fonts = {
+      monospace = {
+        package = (pkgs.nerdfonts.override { fonts = [ "JetBrainsMono" ]; });
+        name = "JetBrainsMonoNL Nerd Font Mono";
+      };
+    };
   };
 }
