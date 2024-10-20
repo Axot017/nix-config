5e9978cc2adbc9d19f88314d456ebec49b2b24e2
Mateusz Ledwoń <mateuszledwon@duck.com>
2024-10-20 12:25:53 +0200

    299 current  2024-10-20 12:25:40  24.11.20241004.bc947f5   6.6.53                          *

--- modules/home-manager/hyprland/swaync.nix
+++ modules/home-manager/hyprland/swaync.nix
@@ -63,6 +63,7 @@
     };
     style = ''
       @define-color bg ${config.lib.stylix.colors.withHashtag.base00};
+      @define-color bg80 ${config.lib.stylix.colors.withHashtag.base00}cc;
       @define-color border-color ${config.lib.stylix.colors.withHashtag.base0D};
       @define-color noti-bg rgb(17, 17, 27);
       @define-color noti-bg-darker rgb(43, 43, 57);
@@ -217,8 +218,7 @@
       }
 
       .control-center {
-        opacity: 0.8;
-        background: @bg;
+        background: @bg80;
         border: 2px solid @border-color;
         border-radius: 8px;
       }
