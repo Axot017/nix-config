f96e543c3409857de5c3040dfb5918a54dfe36a1
Mateusz Ledwoń <mateuszledwon@duck.com>
2024-11-03 21:10:46 +0100

    346 current  2024-11-03 21:10:31  24.11.20241029.807e915   6.6.58                          *

--- modules/home-manager/nix-vim/plugins/mini.nix
+++ modules/home-manager/nix-vim/plugins/mini.nix
@@ -1,6 +1,13 @@
 { pkgs, config, inputs, ... }: {
   programs.nixvim.plugins.mini = {
     enable = true;
-    modules = { files = { }; };
+    modules = {
+      files = {
+        mappings = {
+          reveal_cwd = "<leader>.";
+          close = "<esc>";
+        };
+      };
+    };
   };
 }
