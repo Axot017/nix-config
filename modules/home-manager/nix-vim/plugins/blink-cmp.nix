27922d88a2638ae2d6f998e2bca566de7312cdcb
Mateusz Ledwoń <mateuszledwon@duck.com>
2024-12-29 13:16:40 +0100

    413 current  2024-12-29 13:16:22  25.05.20241227.634fd46   6.6.67                          *

--- modules/home-manager/nix-vim/plugins/blink-cmp.nix
+++ modules/home-manager/nix-vim/plugins/blink-cmp.nix
@@ -1,6 +1,9 @@
 { pkgs, config, inputs, ... }: {
   programs.nixvim.plugins.blink-cmp = {
     enable = true;
-    settings = { keymap.preset = "enter"; };
+    settings = {
+      keymap.preset = "super-tab";
+      completion.list.selection = "auto_insert";
+    };
   };
 }
