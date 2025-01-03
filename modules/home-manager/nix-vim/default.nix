0bc8609ba7af8e0a3ba433a3086b3abca4aec956
Mateusz Ledwoń <mateuszledwon@duck.com>
2025-01-03 23:37:06 +0100

    425 current  2025-01-03 23:36:49  25.05.20241229.88195a9   6.6.68                          *

--- modules/home-manager/nix-vim/default.nix
+++ modules/home-manager/nix-vim/default.nix
@@ -44,8 +44,8 @@
         log_level = 1,
       })
       vim.keymap.set("n", "<leader>mt", multiterm.bind_toggle)
-      vim.keymap.set("n", "<leader>mr", multiterm.bind_reset_session)
-      vim.keymap.set("n", "<leader>ma", multiterm.close_all)
+      vim.keymap.set("n", "<leader>mr", multiterm.bind_remove)
+      vim.keymap.set("n", "<leader>ma", multiterm.remove_all)
 
 
     '';
