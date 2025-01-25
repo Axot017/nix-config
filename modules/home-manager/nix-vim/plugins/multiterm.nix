bf0988984b13f53a0d9c408439012b0c7bdbbda8
Mateusz Ledwoń <mateuszledwon@duck.com>
2025-01-25 14:25:46 +0100

    433 current  2025-01-25 14:25:28  25.05.20250123.0aa4755   6.12.10                          *

--- modules/home-manager/nix-vim/plugins/multiterm.nix
+++ modules/home-manager/nix-vim/plugins/multiterm.nix
@@ -1,20 +1,20 @@
 { pkgs, config, inputs, ... }: {
-  programs.nixvim.extraPlugins = [
-    (pkgs.vimUtils.buildVimPlugin {
-      name = "multiterm.nvim";
-      dependencies = with pkgs.vimPlugins; [ plenary-nvim ];
-      src = pkgs.fetchFromGitHub {
-        owner = "Axot017";
-        repo = "multiterm.nvim";
-        rev = "master";
-        sha256 = "sha256-yN46X/8BvgDlD7gwQuaktyqGFq7VHP0lTddqAUeJjzQ=";
-      };
-    })
-  ];
+  # programs.nixvim.extraPlugins = [
+  #   (pkgs.vimUtils.buildVimPlugin {
+  #     name = "multiterm.nvim";
+  #     dependencies = with pkgs.vimPlugins; [ plenary-nvim ];
+  #     src = pkgs.fetchFromGitHub {
+  #       owner = "Axot017";
+  #       repo = "multiterm.nvim";
+  #       rev = "master";
+  #       sha256 = "sha256-yN46X/8BvgDlD7gwQuaktyqGFq7VHP0lTddqAUeJjzQ=";
+  #     };
+  #   })
+  # ];
   programs.nixvim.extraConfigLua = ''
-    require('multiterm').setup()
-
     -- For development
-    -- vim.opt.rtp:append(vim.fn.expand('$HOME/Projects/multiterm.nvim'))
+    vim.opt.rtp:append(vim.fn.expand('$HOME/Projects/multiterm.nvim'))
+
+    require('multiterm').setup()
   '';
 }
