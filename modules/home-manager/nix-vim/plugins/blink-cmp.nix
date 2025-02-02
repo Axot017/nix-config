1114700451bfecb7d57054e06e6e4dc778bb4207
Mateusz Ledwoń <mateuszledwon@duck.com>
2025-02-02 21:01:40 +0100

    437 current  2025-02-02 21:01:21  25.05.20250123.0aa4755   6.12.10                          *

--- modules/home-manager/nix-vim/plugins/blink-cmp.nix
+++ modules/home-manager/nix-vim/plugins/blink-cmp.nix
@@ -6,6 +6,7 @@
         preset = "none";
         "<Tab>" = [ "select_next" "fallback" ];
         "<S-Tab>" = [ "select_prev" "fallback" ];
+        "<CR>" = [ "accept" "fallback" ];
       };
       completion.list.selection = {
         auto_insert = true;
