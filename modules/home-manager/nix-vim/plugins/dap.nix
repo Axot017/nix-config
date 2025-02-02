658556fe8146c44e677261c0a811e012e32f9d26
Mateusz Ledwoń <mateuszledwon@duck.com>
2025-02-02 22:22:26 +0100

    438 current  2025-02-02 21:05:47  25.05.20250201.3a22805  6.12.11                          *

--- modules/home-manager/nix-vim/plugins/dap.nix
+++ modules/home-manager/nix-vim/plugins/dap.nix
@@ -1,124 +1,124 @@
 { pkgs, config, inputs, ... }: {
-  programs.nixvim.plugins.dap = {
-    enable = true;
-    adapters = { servers = { }; };
-    configurations = { };
-    extensions = {
-      dap-go = {
-        enable = true;
-        dapConfigurations = [
-          {
-            name = "Launch";
-            type = "go";
-            request = "launch";
-            mode = "auto";
-            program = "\${fileDirname}";
-            env = { };
-            args = [ ];
-          }
-          {
-            name = "Attach to Delve (server)";
-            type = "go";
-            request = "attach";
-            mode = "remote";
-            host = "localhost";
-            port = 3001;
-            cwd = "\${workspaceFolder}";
-            debugAdapter = "dlv-dap";
-            substitutePath = [
-              {
-                from = "\${workspaceFolder}";
-                to = "/project";
-              }
-              {
-                from = "\${env:GOPATH}";
-                to = "/go";
-              }
-            ];
-          }
-          {
-            name = "Attach to Delve (drain)";
-            type = "go";
-            request = "attach";
-            mode = "remote";
-            host = "localhost";
-            port = 3002;
-            cwd = "\${workspaceFolder}";
-            debugAdapter = "dlv-dap";
-            substitutePath = [
-              {
-                from = "\${workspaceFolder}";
-                to = "/project";
-              }
-              {
-                from = "\${env:GOPATH}";
-                to = "/go";
-              }
-            ];
-          }
-          {
-            name = "Attach to Delve (launcher)";
-            type = "go";
-            request = "attach";
-            mode = "remote";
-            host = "localhost";
-            port = 3004;
-            cwd = "\${workspaceFolder}";
-            debugAdapter = "dlv-dap";
-            substitutePath = [
-              {
-                from = "\${workspaceFolder}";
-                to = "/project";
-              }
-              {
-                from = "\${env:GOPATH}";
-                to = "/go";
-              }
-            ];
-          }
-          {
-            name = "Attach to Delve (vcs-gateway)";
-            type = "go";
-            request = "attach";
-            mode = "remote";
-            host = "localhost";
-            port = 3005;
-            cwd = "\${workspaceFolder}";
-            debugAdapter = "dlv-dap";
-            substitutePath = [
-              {
-                from = "\${workspaceFolder}";
-                to = "/project";
-              }
-              {
-                from = "\${env:GOPATH}";
-                to = "/go";
-              }
-            ];
-          }
-          {
-            name = "Attach to Delve (worker)";
-            type = "go";
-            request = "attach";
-            mode = "remote";
-            host = "localhost";
-            port = 3007;
-            cwd = "\${workspaceFolder}";
-            debugAdapter = "dlv-dap";
-            substitutePath = [
-              {
-                from = "\${workspaceFolder}";
-                to = "/project";
-              }
-              {
-                from = "\${env:GOPATH}";
-                to = "/go";
-              }
-            ];
-          }
-        ];
-      };
-      dap-ui = { enable = true; };
+  programs.nixvim.plugins = {
+    dap = {
+      enable = true;
+      adapters = { servers = { }; };
+      configurations = { };
     };
+    dap-go = {
+      enable = true;
+      settings.dap_configurations = [
+        {
+          name = "Launch";
+          type = "go";
+          request = "launch";
+          mode = "auto";
+          program = "\${fileDirname}";
+          env = { };
+          args = [ ];
+        }
+        {
+          name = "Attach to Delve (server)";
+          type = "go";
+          request = "attach";
+          mode = "remote";
+          host = "localhost";
+          port = 3001;
+          cwd = "\${workspaceFolder}";
+          debugAdapter = "dlv-dap";
+          substitutePath = [
+            {
+              from = "\${workspaceFolder}";
+              to = "/project";
+            }
+            {
+              from = "\${env:GOPATH}";
+              to = "/go";
+            }
+          ];
+        }
+        {
+          name = "Attach to Delve (drain)";
+          type = "go";
+          request = "attach";
+          mode = "remote";
+          host = "localhost";
+          port = 3002;
+          cwd = "\${workspaceFolder}";
+          debugAdapter = "dlv-dap";
+          substitutePath = [
+            {
+              from = "\${workspaceFolder}";
+              to = "/project";
+            }
+            {
+              from = "\${env:GOPATH}";
+              to = "/go";
+            }
+          ];
+        }
+        {
+          name = "Attach to Delve (launcher)";
+          type = "go";
+          request = "attach";
+          mode = "remote";
+          host = "localhost";
+          port = 3004;
+          cwd = "\${workspaceFolder}";
+          debugAdapter = "dlv-dap";
+          substitutePath = [
+            {
+              from = "\${workspaceFolder}";
+              to = "/project";
+            }
+            {
+              from = "\${env:GOPATH}";
+              to = "/go";
+            }
+          ];
+        }
+        {
+          name = "Attach to Delve (vcs-gateway)";
+          type = "go";
+          request = "attach";
+          mode = "remote";
+          host = "localhost";
+          port = 3005;
+          cwd = "\${workspaceFolder}";
+          debugAdapter = "dlv-dap";
+          substitutePath = [
+            {
+              from = "\${workspaceFolder}";
+              to = "/project";
+            }
+            {
+              from = "\${env:GOPATH}";
+              to = "/go";
+            }
+          ];
+        }
+        {
+          name = "Attach to Delve (worker)";
+          type = "go";
+          request = "attach";
+          mode = "remote";
+          host = "localhost";
+          port = 3007;
+          cwd = "\${workspaceFolder}";
+          debugAdapter = "dlv-dap";
+          substitutePath = [
+            {
+              from = "\${workspaceFolder}";
+              to = "/project";
+            }
+            {
+              from = "\${env:GOPATH}";
+              to = "/go";
+            }
+          ];
+        }
+      ];
+    };
+    dap-ui = { enable = true; };
   };
 }
