d9ffbb76790b8c61f03dcdd13c2ee5daa7d243a6
Mateusz Ledwoń <mateuszledwon@duck.com>
2025-02-08 18:28:48 +0100

    444 current  2025-02-08 18:28:46  25.05.20250201.3a22805  6.12.11                          *

--- hosts/default/configuration.nix
+++ hosts/default/configuration.nix
@@ -17,6 +17,20 @@
     inputs.home-manager.nixosModules.default
   ];
   services.blueman.enable = true;
+  hardware.bluetooth = {
+    enable = true;
+    powerOnBoot = true;
+    settings = {
+      General = {
+        Name = "Computer";
+        ControllerMode = "dual";
+        FastConnectable = "true";
+        Experimental = "true";
+      };
+      Policy = { AutoEnable = "true"; };
+      LE = { EnableAdvMonInterleaveScan = "true"; };
+    };
+  };
   boot.kernelModules = [ "nct6775" ];
   hardware.graphics = {
     enable = true;
