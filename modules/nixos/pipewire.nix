a0b006accb992d4ab14bfb589ca6d5642600c990
Mateusz Ledwoń <mateuszledwon@duck.com>
2025-02-08 14:45:22 +0100

    442 current  2025-02-08 14:45:19  25.05.20250201.3a22805  6.12.11                          *

--- modules/nixos/pipewire.nix
+++ modules/nixos/pipewire.nix
@@ -11,6 +11,16 @@
       alsa.enable = true;
       alsa.support32Bit = true;
       pulse.enable = true;
+      extraConfig.pipewire.adjust-sample-rate = {
+        "context.properties" = {
+          "default.clock.rate" = 192000;
+          #"defautlt.allowed-rates" = [ 192000 48000 44100 ];
+          "defautlt.allowed-rates" = [ 192000 ];
+          "default.clock.quantum" = 32;
+          "default.clock.min-quantum" = 32;
+          "default.clock.max-quantum" = 32;
+        };
+      };
     };
 
   };
