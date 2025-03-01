7d4223994604a203341a77a929b8cd4a6c9bf0c2
Mateusz Ledwoń <mateuszledwon@duck.com>
2025-03-01 22:52:46 +0100

    491 current  2025-03-01 22:52:42  25.05.20250226.5135c59  6.12.16                          *

--- modules/home-manager/hyprland/ags/config/widget/TopBar.tsx
+++ modules/home-manager/hyprland/ags/config/widget/TopBar.tsx
@@ -313,7 +313,7 @@ const weather = Variable({
   icon: ""
 }).poll(1000, async (old) => {
   try {
-    const key = exec(`cat $HOME/.config/.secret/openweather`).trim()
+    const key = exec(`cat ~/.config/.secret/openweather`).trim()
     console.log("KEY", key)
     const url = `https://api.openweathermap.org/data/2.5/weather?lat=50.281760&lon=18.997510&appid=${key}&units=metric`
     const stdout = exec(`curl -s "${url}" | jq -r '.main.temp, .weather[0].icon'`)
