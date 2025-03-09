a83e616a70359dd009c9bc9ad9dec16007a173df
Mateusz Ledwoń <mateuszledwon@duck.com>
2025-03-09 22:30:42 +0100

    523 current  2025-03-09 22:30:36  25.05.20250303.ba487db  6.12.17                          *

--- modules/home-manager/nix-vim/plugins/multiterm.nix
+++ modules/home-manager/nix-vim/plugins/multiterm.nix
@@ -1,15 +1,15 @@
 { pkgs, config, inputs, ... }: {
   programs.nixvim.extraPlugins = [
-    (pkgs.vimUtils.buildVimPlugin {
-      name = "multiterm.nvim";
-      dependencies = with pkgs.vimPlugins; [ plenary-nvim ];
-      src = pkgs.fetchFromGitHub {
-        owner = "Axot017";
-        repo = "multiterm.nvim";
-        rev = "master";
-        sha256 = "sha256-GMZ2U8Z0nY8W70zJWwxJdvieYrZreapgKksKttg/xQc=";
-      };
-    })
+    # (pkgs.vimUtils.buildVimPlugin {
+    #   name = "multiterm.nvim";
+    #   dependencies = with pkgs.vimPlugins; [ plenary-nvim ];
+    #   src = pkgs.fetchFromGitHub {
+    #     owner = "Axot017";
+    #     repo = "multiterm.nvim";
+    #     rev = "master";
+    #     sha256 = "sha256-GMZ2U8Z0nY8W70zJWwxJdvieYrZreapgKksKttg/xQc=";
+    #   };
+    # })
   ];
   programs.nixvim.extraConfigLua = ''
     -- For development
