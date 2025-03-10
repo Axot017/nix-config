2810db8c3032472091dd2ac52c55ae41eed787bf
Mateusz Ledwoń <mateuszledwon@duck.com>
2025-03-10 22:13:34 +0100

    526 current  2025-03-10 22:13:28  25.05.20250303.ba487db  6.12.17                          *

--- modules/home-manager/nix-vim/plugins/mini.nix
+++ modules/home-manager/nix-vim/plugins/mini.nix
@@ -2,6 +2,7 @@
   programs.nixvim.plugins.mini = {
     enable = true;
     modules = {
+      test = { };
       indentscope = {
         draw = { animation.__raw = "function(s,n) return 10 end"; };
       };
