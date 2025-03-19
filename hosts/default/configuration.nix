6adc7f097a4b11af58eb445db5d434e9fd445635
Mateusz Ledwoń <mateuszledwon@duck.com>
2025-03-19 08:36:35 +0100

    545 current  2025-03-19 08:36:29  25.05.20250313.6607cf7  6.12.18                          *

--- hosts/default/configuration.nix
+++ hosts/default/configuration.nix
@@ -145,20 +145,6 @@
     unrar
     jq
     claude-code
-    (pkgs.python3Packages.buildPythonPackage rec {
-      pname = "vectorcode";
-      version = "0.4.12";
-      format = "wheel";
-      nativeBuildInputs = [ installShellFiles ];
-      src = pkgs.python3Packages.fetchPypi {
-        inherit pname version;
-        sha256 = "sha256-Yu4N7EAGOAVdAxBMTbrO/j5DzDgtlmpxOJ/SFzyI7M4=";
-      };
-
-      postInstall = ''
-        installShellCompletion --cmd vectorcode
-      '';
-    })
   ];
 
   environment.sessionVariables = {
