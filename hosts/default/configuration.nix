6f04723f7f87b07b97adb675e3f580b5da5879b3
Mateusz Ledwoń <mateuszledwon@duck.com>
2025-03-19 08:26:07 +0100

    543 current  2025-03-19 08:26:01  25.05.20250313.6607cf7  6.12.18                          *

--- hosts/default/configuration.nix
+++ hosts/default/configuration.nix
@@ -145,15 +145,19 @@
     unrar
     jq
     claude-code
-    (pkgs.python3Packages.buildPythonPackage rec {
-      pname = "vectorcode";
-      version = "0.4.12";
-      format = "wheel";
-      src = pkgs.python3Packages.fetchPypi {
-        inherit pname version;
-        sha256 = "sha256-Yu4N7EAGOAVdAxBMTbrO/j5DzDgtlmpxOJ/SFzyI7M4=";
-      };
-    })
+
+    (pkgs.python3.withPackages (python-pkgs:
+      [
+        (pkgs.python3Packages.buildPythonPackage rec {
+          pname = "vectorcode";
+          version = "0.4.12";
+          format = "wheel";
+          src = pkgs.python3Packages.fetchPypi {
+            inherit pname version;
+            sha256 = "sha256-Yu4N7EAGOAVdAxBMTbrO/j5DzDgtlmpxOJ/SFzyI7M4=";
+          };
+        })
+      ]))
   ];
 
   environment.sessionVariables = {
