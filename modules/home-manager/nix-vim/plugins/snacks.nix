2b19c89779d728dd3622660982cd80fdfc2cd930
Mateusz Ledwoń <mateuszledwon@duck.com>
2025-03-22 23:43:27 +0100

    574 current  2025-03-22 23:43:20  25.05.20250313.6607cf7  6.12.18                          *

--- modules/home-manager/nix-vim/plugins/snacks.nix
+++ modules/home-manager/nix-vim/plugins/snacks.nix
@@ -1,15 +1,25 @@
 { pkgs, config, inputs, ... }: {
   programs.nixvim.plugins.snacks = {
     enable = true;
+    luaConfig.post = ''
+      vim.api.nvim_create_autocmd("User", {
+        pattern = "OilActionsPost",
+        callback = function(event)
+            if event.data.actions.type == "move" then
+                Snacks.rename.on_rename_file(event.data.actions.src_url, event.data.actions.dest_url)
+            end
+        end,
+      })
+    '';
     settings = {
       animate = { enabled = true; };
       bigfile = { enabled = true; };
       bufdelete = { enabled = true; };
-      dashboard = { enabled = true; };
       image = { enabled = true; };
       indent = { enabled = true; };
       input = { enabled = true; };
       notifier = { enabled = true; };
+      rename = { enabled = true; };
       scope = { enabled = true; };
       scroll = { enabled = true; };
       words = { enabled = true; };
