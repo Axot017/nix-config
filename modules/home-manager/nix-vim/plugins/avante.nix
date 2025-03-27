b6c63d8deeff79ecefff82cab78c5661f763aaf4
Mateusz Ledwoń <mateuszledwon@duck.com>
2025-03-27 22:14:00 +0100

    590 current  2025-03-27 22:13:52  25.05.20250322.1e5b653  6.12.19                          *

--- modules/home-manager/nix-vim/plugins/avante.nix
+++ modules/home-manager/nix-vim/plugins/avante.nix
@@ -2,7 +2,7 @@
   programs.nixvim.plugins.avante = {
     enable = true;
     settings = {
-      provider = "claude";
+      provider = "gemini";
       cursor_applying_provider = "gemini";
       claude = {
         api_key_name = "cmd:cat /home/axot/.config/.secret/anthropic";
