3d8fa5747d2b479e5228889a1cbdb543e5492c13
Mateusz Ledwoń <mateuszledwon@duck.com>
2025-03-27 08:40:35 +0100

    581 current  2025-03-27 08:40:27  25.05.20250322.1e5b653  6.12.19                          *

--- modules/home-manager/nix-vim/plugins/avante.nix
+++ modules/home-manager/nix-vim/plugins/avante.nix
@@ -3,23 +3,12 @@
     enable = true;
     settings = {
       provider = "claude";
-      file_selector = { provider = "snacks"; };
+      claude = {
+        api_key_name = "cmd: cat ~/.config/.secret/anthropic";
+      };
+      file_selector = { 
+        provider = "snacks";
+      };
     };
-    luaConfig.pre = ''
-      function get_secret(vendor)
-        local path = os.getenv("HOME") .. "/.config/.secret/" .. vendor
-        local f = io.open(path, "r")
-        if f == nil then
-          return ""
-        end
-        local key = f:read("*all")
-        f:close()
-
-        return string.gsub(key, "\n", "")
-      end
-
-      os.setenv("GEMINI_API_KEY", get_secret("gemini"))
-      os.setenv("ANTHROPIC_API_KEY", get_secret("anthropic"))
-    '';
   };
 }
