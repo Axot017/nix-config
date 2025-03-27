0727613d2930b7c0aac49c183be86d9f99e90cdb
Mateusz Ledwoń <mateuszledwon@duck.com>
2025-03-27 22:21:05 +0100

    591 current  2025-03-27 22:20:57  25.05.20250322.1e5b653  6.12.19                          *

--- modules/home-manager/nix-vim/plugins/avante.nix
+++ modules/home-manager/nix-vim/plugins/avante.nix
@@ -2,17 +2,18 @@
   programs.nixvim.plugins.avante = {
     enable = true;
     settings = {
-      provider = "gemini";
+      provider = "claude";
       cursor_applying_provider = "gemini";
       claude = {
         api_key_name = "cmd:cat /home/axot/.config/.secret/anthropic";
+        model = "claude-3-7-sonnet-latest";
       };
       gemini = {
         api_key_name = "cmd:cat /home/axot/.config/.secret/gemini";
         model = "gemini-2.0-flash";
       };
       behaviour = {
-        enable_cursor_planning_mode = true;
+        enable_cursor_planning_mode = false;
         auto_suggestions = false;
       };
       file_selector = { provider = "snacks"; };
