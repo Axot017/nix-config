48313fd39a8a2f8dd357e51e4d78c87d8e8feb28
Mateusz Ledwoń <mateuszledwon@duck.com>
2025-03-31 17:31:54 +0200

    607 current  2025-03-31 17:31:46  25.05.20250322.1e5b653  6.12.19                          *

--- modules/home-manager/nix-vim/plugins/minuet.nix
+++ modules/home-manager/nix-vim/plugins/minuet.nix
@@ -16,10 +16,11 @@
             accept = '<C-y>',
           },
         },
-        provider = "gemini",
+        provider = "qwen",
         throttle = 0,
         debounce = 150,
         n_completions = 1,
+        context_window = 512,
         provider_options = {
           gemini = {
             api_key = function()
@@ -34,6 +35,15 @@
               return string.gsub(key, "\n", "")
             end,
             optional = {},
+          },
+          qwen = {
+              name = 'Ollama',
+              end_point = 'http://localhost:11434/v1/completions',
+              model = 'qwen2.5-coder:7b',
+              optional = {
+                  max_tokens = 56,
+                  top_p = 0.9,
+              },
           },
         }
       })
