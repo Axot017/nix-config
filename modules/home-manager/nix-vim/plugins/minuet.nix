6d51ef9ed410678c9a9e64f06a67ad758780c7e9
Mateusz Ledwoń <mateuszledwon@duck.com>
2025-03-31 17:35:47 +0200

    608 current  2025-03-31 17:35:38  25.05.20250322.1e5b653  6.12.19                          *

--- modules/home-manager/nix-vim/plugins/minuet.nix
+++ modules/home-manager/nix-vim/plugins/minuet.nix
@@ -16,7 +16,7 @@
             accept = '<C-y>',
           },
         },
-        provider = "qwen",
+        provider = "openai_fim_compatible",
         throttle = 0,
         debounce = 150,
         n_completions = 1,
@@ -36,14 +36,15 @@
             end,
             optional = {},
           },
-          qwen = {
-              name = 'Ollama',
-              end_point = 'http://localhost:11434/v1/completions',
-              model = 'qwen2.5-coder:7b',
-              optional = {
-                  max_tokens = 56,
-                  top_p = 0.9,
-              },
+          openai_fim_compatible = {
+            name = 'Ollama',
+            api_key = 'TERM',
+            end_point = 'http://localhost:11434/v1/completions',
+            model = 'qwen2.5-coder:7b',
+            optional = {
+              max_tokens = 56,
+              top_p = 0.9,
+            },
           },
         }
       })
