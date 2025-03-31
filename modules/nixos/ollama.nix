5d1595b62dbb0a4c7f1ceecdb8226046e5eeea31
Mateusz Ledwoń <mateuszledwon@duck.com>
2025-03-31 20:36:26 +0200

    614 current  2025-03-31 20:36:17  25.05.20250322.1e5b653  6.12.19                          *

--- modules/nixos/ollama.nix
+++ modules/nixos/ollama.nix
@@ -1,4 +1,9 @@
 { config, pkgs, inputs, ... }: {
   nixpkgs.config.rocmSupport = true;
-  services = { ollama = { enable = true; }; };
+  services = {
+    ollama = {
+      enable = true;
+      rocmOverrideGfx = "10.1.0";
+    };
+  };
 }
