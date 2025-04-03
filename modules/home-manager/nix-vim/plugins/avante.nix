a668aa6b8f219890086a07bf1e0d9feb4c913255
Mateusz Ledwoń <mateuszledwon@duck.com>
2025-04-03 21:23:27 +0200

    637 current  2025-04-03 21:23:17  25.05.20250322.1e5b653  6.12.19                          *

--- modules/home-manager/nix-vim/plugins/avante.nix
+++ modules/home-manager/nix-vim/plugins/avante.nix
@@ -24,7 +24,7 @@
         ollama = {
           __inherited_from = "openai";
           api_key_name = "";
-          endpoint = "http://127.0.0.1:11434/v1";
+          endpoint = "http://127.0.0.1:11434";
           model = "qwen2.5-coder:7b";
           disable_tools = true;
         };
