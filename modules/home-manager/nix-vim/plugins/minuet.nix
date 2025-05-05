798c0c37f6449141f5ae4fd44b3e5a85a41bdb5a
Mateusz Ledwoń <mateuszledwon@duck.com>
2025-05-05 20:34:32 +0200

    652 current  2025-05-05 20:34:29  25.05.20250423.8a2f738  6.12.24                          *

--- modules/home-manager/nix-vim/plugins/minuet.nix
+++ modules/home-manager/nix-vim/plugins/minuet.nix
@@ -22,7 +22,7 @@
         throttle = 200,
         debounce = 150,
         n_completions = 1,
-        context_window = 512,
+        context_window = 4096,
         provider_options = {
           gemini = {
             api_key = function()
