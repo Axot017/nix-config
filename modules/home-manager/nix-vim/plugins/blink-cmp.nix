44bd09d7dc783e2ec36d403783a835c476e0e6ac
Mateusz Ledwoń <mateuszledwon@duck.com>
2025-05-08 21:11:21 +0200

    660 current  2025-05-08 21:11:07  25.05.20250505.3730d8a  6.12.26                          *

--- modules/home-manager/nix-vim/plugins/blink-cmp.nix
+++ modules/home-manager/nix-vim/plugins/blink-cmp.nix
@@ -1,15 +1,4 @@
 { pkgs, config, inputs, ... }: {
-  programs.nixvim.extraPlugins = [
-    (pkgs.vimUtils.buildVimPlugin {
-      name = "blink-cmp-avante";
-      src = pkgs.fetchFromGitHub {
-        owner = "Kaiser-Yang";
-        repo = "blink-cmp-avante";
-        rev = "master";
-        sha256 = "sha256-1ltVpWN+JEAdC6g8IIGfTEpTflpuZ/jbN4ByXbjhPEU=";
-      };
-    })
-  ];
   programs.nixvim.plugins.blink-cmp = {
     enable = true;
     settings = {
@@ -18,16 +7,6 @@
         "<Tab>" = [ "select_next" "fallback" ];
         "<S-Tab>" = [ "select_prev" "fallback" ];
         "<CR>" = [ "accept" "fallback" ];
-      };
-      sources = {
-        default = [ "avante" "lsp" "path" "snippets" "buffer" ];
-        providers = {
-          avante = {
-            module = "blink-cmp-avante";
-            name = "Avante";
-            opts = { };
-          };
-        };
       };
       completion.list.selection = {
         auto_insert = true;
