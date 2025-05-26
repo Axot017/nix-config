006ce304fa60084bb497cb3c0c98b76d81e0b219
Mateusz Ledwoń <mateuszledwon@duck.com>
2025-05-26 07:41:12 +0200

    672 current  2025-05-26 07:41:09  25.11.20250523.063f43f  6.12.30                          *

--- modules/home-manager/nix-vim/plugins/fateweaver.nix
+++ modules/home-manager/nix-vim/plugins/fateweaver.nix
@@ -1,14 +1,13 @@
-{ pkgs, config, inputs, ... }:
-{
-  # programs.nixvim.extraConfigLua = ''
-  #   -- For development
-  #   vim.opt.rtp:append(vim.fn.expand('$HOME/Projects/fateweaver.nvim'))
-  #
-  #   local fateweaver = require('fateweaver')
-  #
-  #   fateweaver.setup({
-  #     log_level = 'DEBUG',
-  #     logger_fn = require('fateweaver.logger').file_logger(vim.fn.stdpath("cache") .. "/fateweaver.log")
-  #   })
-  # '';
+{ pkgs, config, inputs, ... }: {
+  programs.nixvim.extraConfigLua = ''
+    -- For development
+    vim.opt.rtp:append(vim.fn.expand('$HOME/Projects/fateweaver.nvim'))
+
+    local fateweaver = require('fateweaver')
+
+    fateweaver.setup({
+      log_level = 'DEBUG',
+      logger_fn = require('fateweaver.logger').file_logger(vim.fn.stdpath("cache") .. "/fateweaver.log")
+    })
+  '';
 }
