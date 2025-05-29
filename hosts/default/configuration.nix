c3d3b6452ac801abb19768e70ff3dc8db89a2e46
Mateusz Ledwoń <mateuszledwon@duck.com>
2025-05-29 09:39:33 +0200

    689 current  2025-05-29 09:39:29  25.11.20250523.063f43f  6.12.30                          *

--- hosts/default/configuration.nix
+++ hosts/default/configuration.nix
@@ -36,6 +36,13 @@
     enable = true;
     enable32Bit = true;
   };
+  systemd.tmpfiles.rules = let
+    rocmEnv = pkgs.symlinkJoin {
+      name = "rocm-combined";
+      paths = with pkgs.rocmPackages; [ rocblas hipblas clr ];
+    };
+  in [ "L+    /opt/rocm   -    -    -     -    ${rocmEnv}" ];
+  hardware.graphics.extraPackages = with pkgs; [ rocmPackages.clr.icd ];
 
   # Bootloader.
   boot.loader.systemd-boot.enable = true;
