30b662fe3fe0fd78543cd9931623521cefbe04c8
Mateusz Ledwoń <mateuszledwon@duck.com>
2025-05-29 09:05:00 +0200

    687 current  2025-05-29 09:04:56  25.11.20250523.063f43f  6.12.30                          *

--- modules/home-manager/nix-vim/plugins/lualine.nix
+++ modules/home-manager/nix-vim/plugins/lualine.nix
@@ -12,7 +12,10 @@
             require("noice").api.statusline.mode.get,
             cond = require("noice").api.statusline.mode.has,
             color = { fg = "#ff9e64" },
-          }
+          },
+          'encoding', 
+          'fileformat', 
+          'filetype',
         }
       '';
     };
