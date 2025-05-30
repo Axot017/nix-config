06de7179c5adf1cafdd71f540bfd11b491d18974
Mateusz Ledwoń <mateuszledwon@duck.com>
2025-05-30 21:43:06 +0200

    692 current  2025-05-30 15:59:03  25.11.20250523.063f43f  6.12.30                          *

---
+++ modules/home-manager/scripts/run-llamafile.nix
@@ -0,0 +1,52 @@
+{ pkgs, ... }:
+let
+  rocmtoolkit_joined = pkgs.symlinkJoin {
+    name = "rocm-merged";
+
+    paths = with pkgs.rocmPackages; [
+      rocm-core
+      clr
+      rccl
+      miopen
+      miopengemm
+      rocrand
+      rocblas
+      rocsparse
+      hipsparse
+      rocthrust
+      rocprim
+      hipcub
+      roctracer
+      rocfft
+      rocsolver
+      hipfft
+      hipsolver
+      hipblas
+      rocminfo
+      rocm-thunk
+      rocm-comgr
+      rocm-device-libs
+      rocm-runtime
+      clr.icd
+      hipify
+    ];
+  };
+in pkgs.writeShellScriptBin "run-llamafile" ''
+  if [ $# -eq 0 ] ; then
+    echo "Usage: $0 wizardcoder.llamafile -ngl 9999"
+    exit 2
+  fi
+
+  export PATH=${rocmtoolkit_joined}/bin
+  export PATH=$PATH:${pkgs.clang}/bin
+  export PATH=$PATH:${pkgs.coreutils}/bin
+  export PATH=$PATH:${pkgs.gzip}/bin
+
+  export HIPCC_COMPILE_FLAGS_APPEND="-I${rocmtoolkit_joined}/include -I${rocmtoolkit_joined}/include/rocblas"
+  export HIPCC_LINK_FLAGS_APPEND="-L${rocmtoolkit_joined}/lib"
+
+  export ROCM_PATH=${rocmtoolkit_joined}
+  export ROCM_SOURCE_DIR=${rocmtoolkit_joined}
+
+  ${pkgs.bash}/bin/sh "$@"
+''
