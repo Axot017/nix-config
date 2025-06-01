1a3b59bd601b3b48d323e3b5b9bce6ff5ca940ea
Mateusz Ledwoń <mateuszledwon@duck.com>
2025-06-01 14:46:29 +0200

    701 current  2025-06-01 14:46:27  25.11.20250528.96ec055  6.12.30                          *

--- modules/home-manager/nix-vim/plugins/fateweaver.nix
+++ modules/home-manager/nix-vim/plugins/fateweaver.nix
@@ -1,7 +1,29 @@
 { pkgs, config, inputs, ... }: {
+  programs.nixvim.extraPlugins = [
+    (pkgs.vimUtils.buildVimPlugin {
+      name = "multiterm.nvim";
+      dependencies = with pkgs.vimPlugins; [ plenary-nvim ];
+      src = pkgs.fetchFromGitHub {
+        owner = "Axot017";
+        repo = "fateweaver.nvim";
+        rev = "master";
+        sha256 = "sha256-Jlf072IEIsEi21JW7mS39Lj+r0PjltBWLrWetUiw5ts=";
+      };
+    })
+  ];
+  programs.nixvim.keymaps = [{
+    mode = "i";
+    key = "<C-y>";
+    action.__raw = ''
+      function()
+        require('fateweaver').accept_completion()
+      end
+    '';
+    options = { silent = true; };
+  }];
   programs.nixvim.extraConfigLua = ''
     -- For development
-    vim.opt.rtp:append(vim.fn.expand('$HOME/Projects/fateweaver.nvim'))
+    -- vim.opt.rtp:append(vim.fn.expand('$HOME/Projects/fateweaver.nvim'))
 
     local fateweaver = require('fateweaver')
 
