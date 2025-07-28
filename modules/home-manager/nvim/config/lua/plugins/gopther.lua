5e2e8299db1f1d893fcc876ea891f625cb116b8e
Mateusz Ledwoń <mateuszledwon@duck.com>
2025-07-28 12:44:39 +0200

    794         2025-07-28 12:44:37  25.11.20250708.9807714  6.12.36  Unknown                 []              True

---
+++ modules/home-manager/nvim/config/lua/plugins/gopther.lua
@@ -0,0 +1,9 @@
+require('gopher').setup({
+  commands = {
+    iferr = "iferr",
+    go = "go",
+    gomodifytags = "gomodifytags",
+    gotests = "gotests",
+    impl = "impl",
+  }
+})
--- modules/home-manager/nvim/default.nix
+++ modules/home-manager/nvim/default.nix
@@ -4,6 +4,19 @@
   inputs,
   ...
 }:
+let
+  gopher = (
+    pkgs.vimUtils.buildVimPlugin {
+      name = "gopher.nvim";
+      src = pkgs.fetchFromGitHub {
+        owner = "olexsmir";
+        repo = "gopher.nvim";
+        rev = "master";
+        sha256 = "sha256-8F8hi29YJSmu/TdfobznE1jM3grUqdr0MBOp7ZlyCTA=";
+      };
+    }
+  );
+in
 {
   home.file.".config/nvim".source = ./config;
   programs.neovim = {
@@ -25,6 +38,11 @@
       oil-nvim
       snacks-nvim
       tokyonight-nvim
+      gopher
+      nvim-dap
+      nvim-dap-go
+      nvim-dap-ui
+      nvim-nio
     ];
   };
   home.packages = with pkgs; [
@@ -33,5 +51,12 @@
     elixir-ls
     nixfmt-rfc-style
     lua-language-server
+
+    go
+    iferr
+    gomodifytags
+    gotests
+    impl
+    delve
   ];
 }
