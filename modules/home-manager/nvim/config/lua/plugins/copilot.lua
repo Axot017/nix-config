51b1a9be5cbad9f1fc96bc5ea839687b2c9c8770
Mateusz Ledwoń <mateuszledwon@duck.com>
2025-07-28 20:14:08 +0200

    803         2025-07-28 20:14:06  25.11.20250708.9807714  6.12.36  Unknown                 []              True

--- modules/home-manager/nvim/config/lua/plugins/copilot.lua
+++ modules/home-manager/nvim/config/lua/plugins/copilot.lua
@@ -1,11 +1,9 @@
 require('copilot').setup({
   keymap = {
     accept = "<C-y>",
-    accept_line = true,
   },
   suggestion = {
     enable = true,
     auto_trigger = true,
-    trigger_on_accept = true,
   }
 })
