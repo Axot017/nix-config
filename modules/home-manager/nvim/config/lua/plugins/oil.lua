6ab7c0afade47e4aebd1cda38605feb7536ab6c5
Mateusz Ledwoń <mateuszledwon@duck.com>
2025-07-28 11:42:45 +0200

    783         2025-07-28 11:42:42  25.11.20250708.9807714  6.12.36  Unknown                 []              True

--- modules/home-manager/nvim/config/lua/plugins/oil.lua
+++ modules/home-manager/nvim/config/lua/plugins/oil.lua
@@ -1,1 +1,11 @@
-require('oil').setup()
+require('oil').setup({
+  default_file_explorer = true,
+  columns = { "icon" },
+  skip_confirm_for_simple_edits = true,
+  lsp_file_methods = {
+    autosave_changes = true,
+  },
+  view_options = {
+    show_hidden = true,
+  },
+})
