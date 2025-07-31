b4149e192ffe808c134391aa67e7ad9e6c26eab7
Mateusz Ledwoń <mateuszledwon@duck.com>
2025-07-31 21:45:27 +0200

    814         2025-07-31 21:45:24  25.11.20250708.9807714  6.12.36  Unknown                 []              True

--- modules/home-manager/nvim/config/lua/plugins/kulala.lua
+++ modules/home-manager/nvim/config/lua/plugins/kulala.lua
@@ -1,3 +1,8 @@
+local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
+parser_config.kulala_http = {
+  filetype = "http",
+}
+
 require("kulala").setup({
   global_keymaps = false,
 })
