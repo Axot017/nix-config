a966eef5f449596f97029613cab36699c8640e53
Mateusz Ledwoń <mateuszledwon@duck.com>
2025-09-26 13:13:55 +0200

    831         2025-09-26 13:13:52  25.11.20250924.e643668  6.12.48  Unknown                 []              True

--- modules/home-manager/ghostty.nix
+++ modules/home-manager/ghostty.nix
@@ -7,7 +7,8 @@
       enable = true;
       settings = {
         custom-shader = [
-          "${config.home.homeDirectory}/.config/ghostty/shaders/cursor_blaze.glsl"
+          "${config.home.homeDirectory}/.config/ghostty/shaders/cursor_smear.glsl"
+          "${config.home.homeDirectory}/.config/ghostty/shaders/cubes.glsl"
         ];
       };
     };
