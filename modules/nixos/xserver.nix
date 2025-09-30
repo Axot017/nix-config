3bca81ed61bd790add2a43ad7635a59a9a288df6
Mateusz Ledwoń <mateuszledwon@duck.com>
2025-09-30 21:47:22 +0200

    837         2025-09-30 21:47:20  25.11.20250924.e643668  6.12.48  Unknown                 []              True

--- modules/nixos/xserver.nix
+++ modules/nixos/xserver.nix
@@ -1,11 +1,24 @@
-{ config, pkgs, inputs, ... }: {
+{
+  config,
+  pkgs,
+  inputs,
+  ...
+}:
+{
+  environment.systemPackages = with pkgs; [
+    where-is-my-sddm-theme
+  ];
   services = {
     xserver.enable = true;
     displayManager.sddm = {
       enable = true;
       wayland.enable = true;
-      theme = "${import ./packages/sddm-chili.nix { inherit pkgs; }}";
-      settings = { General = { DefaultSession = "hyprland.desktop"; }; };
+      theme = "where_is_my_sddm_theme";
+      settings = {
+        General = {
+          DefaultSession = "hyprland.desktop";
+        };
+      };
     };
   };
 }
