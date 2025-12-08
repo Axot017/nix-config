c1d74396167a74e09fdcb7e45e48a0dc61e366f2
Mateusz Ledwoń <mateuszledwon@duck.com>
2025-12-08 08:34:20 +0100

    862         2025-12-08 08:34:16  25.11.20251117.89c2b23  6.12.58  Unknown                 []              True

--- hosts/default/configuration.nix
+++ hosts/default/configuration.nix
@@ -185,7 +185,7 @@
     kubernetes-helm
     argocd
     openssl
-    google-cloud-sdk
+    (google-cloud-sdk.withExtraComponents [ google-cloud-sdk.components.gke-gcloud-auth-plugin ])
   ];
   nixpkgs.config.permittedInsecurePackages = [ "beekeeper-studio-5.3.4" ];
 
